ai/rake-completion

commit b2cfea263483f439b2e6a135c804cc2e46f354de

Add seek in parent directories
Closes #11

rake
@@ -8,13 +8,20 @@
 _rakecomplete() {
     local cur
     _get_comp_words_by_ref -n : cur
-    rakefile=`find . -maxdepth 1 -iname Rakefile`
+    rakefile=""
+    seek_path="."
+    while true; do
+        rakefile=`find ${seek_path} -maxdepth 1 -iname Rakefile`
+        [[ $(readlink -f $seek_path) != "/" && "$rakefile" == "" ]] || break
+        seek_path="../${seek_path}"
+    done
+
     if [ "$rakefile" != "" ]; then
-        recent=`ls -t .rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
+        recent=`ls -t $seek_path/.rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
         if [[ $recent != '.rake_tasks~' ]]; then
-            rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > .rake_tasks~
+            rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > $seek_path/.rake_tasks~
         fi
-        COMPREPLY=($(compgen -W "`cat .rake_tasks~`" -- "$cur"))
+        COMPREPLY=($(compgen -W "`cat $seek_path/.rake_tasks~`" -- "$cur"))
         # remove colon containing prefix from COMPREPLY items
         __ltrim_colon_completions "$cur"
         return 0

commit 7b4b6cf52ad9da59ad0d82827678524c834b4e80

rake: fix cached tasks lookup
It seems that b2cfea263483f439b2e6a135c804cc2e46f354de broke it,
so it was generating cache on every single run, making it slow.

rake
@@ -18,7 +18,7 @@ _rakecomplete() {
 
     if [[ -f "$rakefile" ]]; then
         recent=`ls -t $seek_path/.rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
-        if [[ $recent != '.rake_tasks~' ]]; then
+        if [[ $recent != "$seek_path/.rake_tasks~" ]]; then
             rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > $seek_path/.rake_tasks~
         fi
         COMPREPLY=($(compgen -W "`cat $seek_path/.rake_tasks~`" -- "$cur"))