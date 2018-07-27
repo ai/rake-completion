ai/rake-completion

commit b2cfea263483f439b2e6a135c804cc2e46f354de

Add seek in parent directories
Closes #11

rake
@@ -8,13 +8,20 @@
 _rakecomplete() {
     local cur
     _get_comp_words_by_ref -n : cur
-    rakefile=`find . -maxdepth 1 -iname Rakefile`
+    rakefile=""
+    seek_path="."
+    while true; do
+        rakefile=`find ${seek_path} -maxdepth 1 -iname Rakefile`
+        [[ $(readlink -f $seek_path) != "/" && "$rakefile" == "" ]] || break
+        seek_path="../${seek_path}"
+    done
+
     if [ "$rakefile" != "" ]; then
-        recent=`ls -t .rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
+        recent=`ls -t $seek_path/.rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
         if [[ $recent != '.rake_tasks~' ]]; then
-            rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > .rake_tasks~
+            rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > $seek_path/.rake_tasks~
         fi
-        COMPREPLY=($(compgen -W "`cat .rake_tasks~`" -- "$cur"))
+        COMPREPLY=($(compgen -W "`cat $seek_path/.rake_tasks~`" -- "$cur"))
         # remove colon containing prefix from COMPREPLY items
         __ltrim_colon_completions "$cur"
         return 0

commit 745e3cb7bbb4f1295881236ff3057377539ae1d0

Performance improvements and test fixes (#15)
* rake: replace find with simple bash file check

This should make autocompletion faster and skip find dependency.

* rake: fix cached tasks lookup

It seems that b2cfea263483f439b2e6a135c804cc2e46f354de broke it,
so it was generating cache on every single run, making it slow.

* [test] run: load bash completion before running tests

Otherwise it throws errors, that certain functions are not found.

* [test] run: remove whitespace

* [test] Remove outdates simple test

As 5def41f5c9803b64bd1e32a5676c0bdc193907a1 enabled showing tasks
without description.

* rake: remove unneeded condition

It should be removed in b2cfea263483f439b2e6a135c804cc2e46f354de.

* rake: make sure we return empty completion if Rakefile not found

* [test] no_rakefile: initial commit

If Rakefile is not found until /, we should return empty completion.

* [test] run: add sleep between touching fresher files

Otherwise use_cache seems to be flaky

* [test] run: add suport for autocomplete from subdirectory

* [test] run_from_subdirectory: initial commit

rake
@@ -11,20 +11,22 @@ _rakecomplete() {
     rakefile=""
     seek_path="."
     while true; do
-        rakefile=`find ${seek_path} -maxdepth 1 -iname Rakefile`
-        [[ $(readlink -f $seek_path) != "/" && "$rakefile" == "" ]] || break
+        rakefile="${seek_path}/Rakefile"
+        [[ $(readlink -f $seek_path) == "/" || -f "$rakefile" ]] && break
         seek_path="../${seek_path}"
     done
 
-    if [ "$rakefile" != "" ]; then
+    if [[ -f "$rakefile" ]]; then
         recent=`ls -t $seek_path/.rake_tasks~ ${rakefile} **/*.rake 2> /dev/null | head -n 1`
-        if [[ $recent != '.rake_tasks~' ]]; then
+        if [[ $recent != "$seek_path/.rake_tasks~" ]]; then
             rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > $seek_path/.rake_tasks~
         fi
         COMPREPLY=($(compgen -W "`cat $seek_path/.rake_tasks~`" -- "$cur"))
         # remove colon containing prefix from COMPREPLY items
         __ltrim_colon_completions "$cur"
         return 0
+    else
+      COMPREPLY=()
     fi
 }
 

test/no_rakefile/description
@@ -1,0 +1,1 @@
+don't do anything if Rake file not found

test/run
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Run all tests and print results.
 
+[ -r /usr/share/bash-completion/bash_completion   ] && . /usr/share/bash-completion/bash_completion
+
+# Run all tests and print results.
 GOOD="\e[32m"
 BAD="\e[31m"
 CLEAR="\e[0m"
@@ -18,13 +20,20 @@ done
 for fresher in */fresher; do
     for file in `cat "$fresher"`; do
         touch "`dirname $fresher`/$file"
+        sleep 0.001
     done
 done
 
 for test in */; do
     cd $test
-    _rakecomplete
-    
+    if [ -d ./subdirectory ]; then
+        cd ./subdirectory
+        _rakecomplete
+        cd ..
+    else
+      _rakecomplete
+    fi
+
     if [ "`echo ${COMPREPLY[@]}`" = "`cat tasks`" ]; then
         echo -e "$GOOD* `cat description`$CLEAR"
     else
@@ -33,7 +42,7 @@ for test in */; do
         echo -e "       got: $BAD${COMPREPLY[@]}$CLEAR"
         let "fail += 1"
     fi
-    
+
     let "all += 1"
     cd ..
 done

test/run_from_subdirectory/Rakefile
@@ -1,0 +1,2 @@
+desc "Task in subdirectory"
+task "subdirectory" do; end

test/run_from_subdirectory/description
@@ -1,0 +1,1 @@
+seek for Rakefile in parent directories

test/run_from_subdirectory/subdirectory/.rake_tasks~
@@ -1,0 +1,1 @@
+subdirectory

test/run_from_subdirectory/tasks
@@ -1,0 +1,1 @@
+subdirectory

test/simple/Rakefile
@@ -1,7 +1,0 @@
-desc "First task"
-task "first" do; end
-
-desc "Second task"
-task "second" do; end
-
-task "hidden" do; end

test/simple/description
@@ -1,1 +1,0 @@
-show all tasks with description

test/simple/tasks
@@ -1,1 +1,0 @@
-first second